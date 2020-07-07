b395f4d45b42841f05c4bbc47203ddb94a362e01
Bailey Segail <bails.seegs@gmail.com>
2020-07-07 19:57:43 +1000

    feat: mailchimp

--- pages/state.tsx
+++ pages/state.tsx
@@ -1,13 +1,11 @@
-import Home, {textState} from "../views/Home";
+import Home from "../views/Home";
 import {useRecoilState} from "recoil/dist";
 
 export default function StatePage() {
-    const [text, setText] = useRecoilState(textState);
 
     return (
         <div>
             <h1>Does state work?</h1>
-            <p>Text state: {text}</p>
         </div>
     )
 }
--- views/Home.tsx
+++ views/Home.tsx
@@ -1,50 +1,48 @@
 import View from "../components/View";
 import Button from "../elements/Button";
 import Form from "../components/Form";
-import Link from "next/link";
-import {atom, useRecoilState} from "recoil/dist";
-
-export const textState = atom({
-    key: 'textState',
-    default: '',
-})
 
 const Home = () => {
-    const [text, setText] = useRecoilState(textState);
-
     const fields = [
         {
             type: 'text',
-            name: 'myInput',
-            value: 'hi'
-        },
-        {
-            type: 'select',
-            name: 'select',
-            value: 'hi',
-            props: {
-                options: [{ label: 'Hi', value: 'hi' }, { label: 'You', value: 'there' }]
-            }
+            name: 'email',
+            value: 'segailbailey+1@gmail.com'
         }
     ]
 
-    const onSubmit = (values: any) => {
-        console.log('values', values)
-        setText(values.myInput)
-    }
+    const onSubmit = () => { }
+
     return (
         <View>
             <h1>My starter pack</h1>
             <h2>TODO:</h2>
             <ul>
-                <li><del>Buttons</del></li>
-                <li><del>Loading state</del></li>
-                <li><del>Forms</del></li>
-                <li><del>Form errors</del></li>
-                <li><del>Form validation</del></li>
-                <li><del>On submit doesnt accept defaults</del></li>
-                <li><del>Form defaults</del></li>
-                <li>Mailchimp Integration</li>
+                <li>
+                    <del>Buttons</del>
+                </li>
+                <li>
+                    <del>Loading state</del>
+                </li>
+                <li>
+                    <del>Forms</del>
+                </li>
+                <li>
+                    <del>Form errors</del>
+                </li>
+                <li>
+                    <del>Form validation</del>
+                </li>
+                <li>
+                    <del>On submit doesnt accept defaults</del>
+                </li>
+                <li>
+                    <del>Form defaults</del>
+                </li>
+                <li>
+                    <del>Mailchimp Integration</del>
+                </li>
+                <li>React Form Hooks</li>
                 <li>Carts</li>
                 <li>Payment (Stripe/PayPal)</li>
                 <li>State Management</li>
@@ -52,8 +50,6 @@ const Home = () => {
                 <li>Error handling</li>
                 <li>Testing</li>
             </ul>
-            <p>My text state: { text }</p>
-            <Link href='/state'><a>Test state</a></Link>
             <Form fields={fields} onSubmit={onSubmit}>
                 <Button type='submit'>Do thing</Button>
             </Form>
